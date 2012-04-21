4a2742e02c057e62be17b2bfd9957797ec1e1dbb
Jamil Draréni <urbeller@gmail.com>
2012-04-21 16:29:30 -0400

    1st class prototypes

--- include/tub.h
+++ include/tub.h
@@ -7,6 +7,36 @@
 
 namespace TUB{
 
+	class Device{
+
+	};
+
+	class Iface{
+
+
+	};
+
+	class Endpoint {
+
+	};
+
+	class PropTalk{
+		private:
+			Device			dev;//The device
+			int				iface;//The interface
+
+		public:
+			PropReq( ){}
+			PropReq( Device _d ) : dev(_d) {}
+			PropReq( Device _d , _iface) : dev(_d) , iface(_iface) {}
+			
+			setDev( Device _d){ dev = _d;}
+			setIface( int _iface){ iface = _iface;}
+
+			send( int prop , int val );
+			get( int prop , int val );
+			get_range( int prop , int &min , int &max );
+	};
 
 };
 
