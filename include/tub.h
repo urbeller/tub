487d60a0ea31d354223443d72c9c38365bd46582
Jamil Draréni <urbeller@gmail.com>
2012-04-23 09:24:39 -0400

    none

--- include/tub.h
+++ include/tub.h
@@ -2,12 +2,48 @@
 #define __TUB__H
 
 
+#include <vector>
+#include <libusb.h>
 #include "tub_enums.h"
 #include "tub_controls.h"
 
+using namespace std;
+
 namespace TUB{
 
+	class Device;
+	class Context;
+	class Iface;
+	class Endpoint;
+	class PropTalk;
+
+	/*This class holds the USB context and list of devices.*/
+	/*Devices can be filtered by class_type...ex: video_class*/
+	class Context{
+		private:
+			libusb_context *ctx ;
+
+		public:
+			vector<Device> list;//Not the best structure if devices must be removed!
+
+
+			int refresh( int class_type=CC_NONE);
+	
+			//ctor
+			Context( ) : ctx(NULL) {}
+			~Context(){
+				if( ctx ){
+					libusb_exit(ctx);
+					free( ctx );
+				}
+			}
+	};
+
 	class Device{
+		private:
+			libusb_device *dev;
+
+		public:
 
 	};
 
@@ -26,16 +62,16 @@ namespace TUB{
 			int				iface;//The interface
 
 		public:
-			PropReq( ){}
-			PropReq( Device _d ) : dev(_d) {}
-			PropReq( Device _d , _iface) : dev(_d) , iface(_iface) {}
+			PropTalk( ){}
+			PropTalk( Device _d ) : dev(_d) {}
+			PropTalk( Device _d , int _iface) : dev(_d) , iface(_iface) {}
 			
-			setDev( Device _d){ dev = _d;}
-			setIface( int _iface){ iface = _iface;}
+			int setDev( Device _d){ dev = _d;}
+			int setIface( int _iface){ iface = _iface;}
 
-			send( int prop , int val );
-			get( int prop , int val );
-			get_range( int prop , int &min , int &max );
+			int send( int prop , int val );
+			int get( int prop , int val );
+			int get_range( int prop , int &min , int &max );
 	};
 
 };
