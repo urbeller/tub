b87f86d2732d35f57dabb892c55284e68f84f445
Jamil Draréni <urbeller@gmail.com>
2012-04-23 18:06:58 -0400

    added a main.cxx

---
+++ examples/main.cxx
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "tub.h"
+
+
+using namespace TUB;
+using namespace std;
+
+int main( int argc , char *argv[] ){
+
+	//Create the usb context.
+	Context ctx;
+
+	//Refresh the device list...with only vide devices.
+	if( ctx.refresh( CC_VIDEO ) != TUB::SUCCESS ){
+		cout<<"Can't initialize libusb.\n";
+		exit(1);
+	}
+
+
+
+	return 0;
+}
